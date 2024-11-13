4552016655651e8cdd428f296c197516874dca30
pratheesh18 <pratheesh1218@gmail.com>
2024-11-13 12:38:13 +0530

    added protected route

--- simple-books/src/router.js
+++ simple-books/src/router.js
@@ -7,12 +7,23 @@ import Dashboard from './components/Dashboard.vue';
 const routes = [
     {path:'/',name : 'Login',component : Login},
     {path:'/signup' , name:'SignUp',component:SignUp},
-    {path:'/dashboard',name:'Dashboard',component:Dashboard}
+    {path:'/dashboard',name:'Dashboard',component:Dashboard,meta:{requiresAuth:true}},
 ];
 
 const router = createRouter({
     history:createWebHistory(),
     routes
 });
+
+router.beforeEach((to,from,next) => {
+    if(to.meta.requiresAuth){
+        const token = localStorage.getItem('token');
+        if(!token){
+            alert('Unauthorized access , Please login');
+            return next({name:'Login'});
+        }
+    }
+    next();
+})
 
 export default router;
