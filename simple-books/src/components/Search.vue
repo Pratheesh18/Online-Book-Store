f618c99bbaabe1050223561e146b467987af947f
pratheesh18 <pratheesh1218@gmail.com>
2024-11-13 16:31:31 +0530

    implemented debounce functionality for search

--- simple-books/src/components/Search.vue
+++ simple-books/src/components/Search.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="mb-4">
-        <input type="text" v-model="query" @input="handleSearch" class="form-control" placeholder="Search by book title ..."/>
+        <input type="text" v-model="query" @input="debouncedSearch" class="form-control" placeholder="Search by book title ..."/>
     </div>
 </template>
 
 <script>
+import { debounce } from '../utils/debounce';
+
 export default {
     data(){
         return{
             query:"",
+            debouncedSearch : null,
         }
+    },
+    created(){
+        this.debouncedSearch = debounce(this.handleSearch,400);
     },
     methods:{
         handleSearch(){
